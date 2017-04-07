e3fedc6d4865258581c5bdb4c4fb73539b63e9da
chenrony <18986691452@189.cn>
2017-04-07 14:29:55 +0800

    date

---
+++ 01.js
@@ -0,0 +1,9 @@
+angular.module('myApp',[])
+.controller('MainController',function($scope,$timeout){
+	$scope.date={};
+	var updateTime=function(){
+		$scope.date.raw=new Date();
+		$timeout(updateTime,1000);
+	}
+	updateTime();
+})
